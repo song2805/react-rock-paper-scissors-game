539178a96fe9820ca680a049e89037f5e8ad2c82
Je <song2805@empas.com>
2024-08-13 08:45:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './App.css';
+import Box from './component/Box'
+
+
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "./img/rock.png",
+  },
+  paper: {
+    name: "Paper",
+    img: "./img/paper.png"
+  },
+  scissors: {
+    name: "scissors",
+    img: "./img/scissors.png"
+  }
+}
+
+function App() {
+  const [userSelector, setUserSelector] = useState(null);
+
+  const play =(userChoice) => {
+    console.log("userSelector", userChoice)
+    setUserSelector(choice[userChoice])
+    
+  }
+  return (
+    <div>
+      <div className='main'>
+        <Box title='You' value={userSelector} />
+        {/* <Box title='Computer'  /> */}
+      </div>
+      <div className='main'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Box.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Box = (props) => {
+    console.log(props)
+  return (
+    <div className='game-box'>
+        <h1>{props.title}</h1>
+        <img src={props.value && props.value.img } />
+        <h2>Win</h2>      
+    </div>
+  )
+}
+
+export default Box
